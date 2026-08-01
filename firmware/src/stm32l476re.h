aafc01be240419fbb161adc7bc471dae32c9b122
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-01 22:43:44 +0100

    done with GPIO register mapping

--- firmware/src/stm32l476re.h
+++ firmware/src/stm32l476re.h
@@ -1,16 +1,45 @@
 #ifndef STM32L476RE_H
 #define STM32L476RE_H
 
-#define PERIPHERAL_BASE    0x40000000
+#include <stdint.h>
 
+#define PERIPH_BASE    0x40000000
+#define GPIOA_BASE     0x48000000 
 typedef struct 
 {
-    volatile uint32_t 
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t OSPEEDR;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t LCKR;
+    volatile uint32_t AFRL;
+    volatile uint32_t AFRH;
+    volatile uint32_t BRR;
+    volatile uint32_t ASCR;
 }GPIO_TypeDef;
 
 typedef struct 
 {
-    /* data */
+    volatile uint32_t CR;
+    volatile uint32_t ICSCR;
+    volatile uint32_t CFGR;
+    volatile uint32_t PLLCFGR;
+    volatile uint32_t PLLSAI1CFGR;
+    volatile uint32_t PLLSAI2CFGR;
+    volatile uint32_t CIER;
+    volatile uint32_t CIFR;
+    volatile uint32_t CICR;
+    volatile uint32_t AHB1RSTR;
+    volatile uint32_t AHB2RSTR;
+    volatile uint32_t 
+    volatile uint32_t 
+    volatile uint32_t 
+    volatile uint32_t 
+    volatile uint32_t 
+    volatile uint32_t 
 }RCC_TypeDef;
 
 #endif
