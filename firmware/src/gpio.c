4e184e87c237580624ce3bf6e07c90428acb6d10
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-02 16:57:51 +0100

    set PC2 and PC3 as inputs

--- firmware/src/gpio.c
+++ firmware/src/gpio.c
@@ -1,5 +1,13 @@
 #include "gpio.h"
 
 void gpio_init(){
-    
+    RCC->AHB2ENR |= (1 << 2); //gpioc enabled
+
+    //setting PC2 and PC3 as inputs
+    GPIOC->MODER &= ~(1 << 2);
+    GPIOC->MODER &= ~(1 << 3);
+
+    GPIOC->MODER &= ~(1 << 4);
+    GPIOC->MODER &= ~(1 << 5);
+
 }
