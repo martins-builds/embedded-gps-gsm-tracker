b4524507774aeb7667e959633ed099931716e09c
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-02 17:26:27 +0100

    finished as pull up cause the buttons connect the pins to ground

--- firmware/src/gpio.c
+++ firmware/src/gpio.c
@@ -4,10 +4,18 @@ void gpio_init(){
     RCC->AHB2ENR |= (1 << 2); //gpioc enabled
 
     //setting PC2 and PC3 as inputs
-    GPIOC->MODER &= ~(1 << 2);
-    GPIOC->MODER &= ~(1 << 3);
-
-    GPIOC->MODER &= ~(1 << 4);
+    GPIOC->MODER &= ~(1 << 4); //PC2
     GPIOC->MODER &= ~(1 << 5);
+    GPIOC->MODER &= ~(1 << 6); //PC3
+    GPIOC->MODER &= ~(1 << 7);
 
+    //setting PC2 and PC3 as pullup
+    GPIOC->PUPDR &= ~(1 << 4); //PC2 clear
+    GPIOC->PUPDR &= ~(1 << 5);
+    GPIOC->PUPDR &= ~(1 << 6); //PC3 clear
+    GPIOC->PUPDR &= ~(1 << 7);
+    GPIOC->PUPDR |= (1 << 4); //PC2 set
+    GPIOC->PUPDR &= ~(1 << 5);
+    GPIOC->PUPDR |= (1 << 6); //PC3 set
+    GPIOC->PUPDR &= ~(1 << 7);
 }
