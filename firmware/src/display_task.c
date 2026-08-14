d99820988b2602ff96c29b59864e9b942f8e517d
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-14 08:54:19 +0100

    Update display_task.c

--- firmware/src/display_task.c
+++ firmware/src/display_task.c
@@ -2,5 +2,14 @@
 #include <stdint.h>
 
 void i2c_init(void){
+    GPIOB->MODER |= (1 << 13); // alternate function modes
+    GPIOB->MODER &= ~(1 << 12);
+    GPIOB->MODER |= (1 << 15);
+    GPIOB->MODER &= ~(1 << 14);
+    GPIOB->OTYPER |= (1 << 6); //open drain output
+    GPIOB->OTYPER |= (1 << 7);
+    RCC->APB1ENR1 |= (1 << 21); // enable 12c1 for display
+
+    I2C->CR1 |= (1 << 0); //periferal enable
     
 }
