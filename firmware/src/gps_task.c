1f0ecfcfb671969d6d52d77fef3b0311b081a489
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-15 06:08:15 +0100

    Starting gps tasks

--- firmware/src/gps_task.c
+++ firmware/src/gps_task.c
@@ -1,2 +1,6 @@
 #include "gps_task.h"
 #include <stdint.h>
+
+void gps_parse_gprmc(const char *sentence, GPS_Data_t *result){
+    
+}
--- firmware/src/gps_task.h
+++ firmware/src/gps_task.h
@@ -1,4 +1,15 @@
 #ifndef GPS_TASK_H
 #define GPS_TASK_H
 
+#include "stm32l476re.h"
+
+typedef struct{
+    float latitude;
+    float longitude;
+    uint8_t hours, minutes, seconds;
+    uint8_t valid;
+} GPS_Data_t;
+
+void gps_parse_gprmc(const char *sentence, GPS_Data_t *result);
+
 #endif
