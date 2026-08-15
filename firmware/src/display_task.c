8194f64b94a715e55e4e16ab1fa6fe20b02fda90
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-15 05:34:58 +0100

    ssd1306 initialization function

--- firmware/src/display_task.c
+++ firmware/src/display_task.c
@@ -95,3 +95,9 @@ void i2c_write(uint8_t dev_addr, uint8_t *data, uint16_t len){
     while (!(I2C1->ISR & (1 << 5)));  // wait for STOPF (stop condition sent)
     I2C1->ICR |= (1 << 5);            // clear STOPF flag
 }
+void ssd1306_init(void){
+    for (uint16_t i = 0; i < sizeof(SSD1306_Init_Sequence); i++) {
+        uint8_t cmd_buf[2] = {0x00, SSD1306_Init_Sequence[i]};
+        i2c_write(0x3C, cmd_buf, 2);
+    }
+}
--- firmware/src/display_task.h
+++ firmware/src/display_task.h
@@ -7,5 +7,6 @@
 void i2c_init(void);
 void OLED_DrawPixel(int16_t x, int16_t y, uint8_t color);
 void i2c_write(uint8_t dev_addr, uint8_t *data, uint16_t len);
+void ssd1306_init(void);
 
 #endif
