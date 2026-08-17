9f48a6c985900ab25811a4d9a01d7805de229047
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-17 07:49:50 +0100

    Update gps_task.h

--- firmware/src/gps_task.h
+++ firmware/src/gps_task.h
@@ -10,6 +10,10 @@ typedef struct{
     uint8_t valid;
 } GPS_Data_t;
 
+#define GPS_BUF_SIZE 100
+extern char gps_rx_buffer[GPS_BUF_SIZE];
+extern volatile uint8_t gps_line_ready;
+
 void gps_parse_gprmc(const char *sentence, GPS_Data_t *result);
 void uart1_init(void);
 
