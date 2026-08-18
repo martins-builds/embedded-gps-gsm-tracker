2900952e96b8b0f50624152acb2dfc95b5cfe954
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-18 12:21:21 +0100

    essentially done

--- firmware/src/gsm_task.c
+++ firmware/src/gsm_task.c
@@ -9,11 +9,10 @@ void uart3_init(void){
     GPIOC->AFRL &= ~((0xF << 16) | (0xF << 20));   // clear both 4-bit fields first
     GPIOC->AFRL |=  ((7 << 16) | (7 << 20));        // AF7 for PA9, PA10
 
-    RCC->APB2ENR |= (1 << 14);
+    RCC->APB1ENR1 |= (1 << 18);
 
-    USART1->BRR = 8333;
-    USART1->CR1 |= (1 << 0) | (1 << 2) | (1 << 3);
+    USART3->BRR = 8333;
+    USART3->CR1 |= (1 << 0) | (1 << 2) | (1 << 3);
 
-    USART1->CR1 |= (1 << 5);      // RXNEIE - enable RX-not-empty interrupt
-    NVIC->ISER[1] |= (1 << 5);   // IRQ37 = USART1
+    NVIC->ISER[1] |= (1 << 7);   // IRQ39 = USART3
 }
