6006b296f3d1f454bb6f9702cd26ff2c66cfaa33
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-18 12:52:14 +0100

    Update gsm_task.h

--- firmware/src/gsm_task.h
+++ firmware/src/gsm_task.h
@@ -4,6 +4,10 @@
 #include "stm32l476re.h"
 #include "core_cm4.h"
 
+#define GSM_BUF_SIZE 200
+extern char gsm_rx_buffer[GSM_BUF_SIZE];
+extern volatile uint8_t gsm_response_ready;
+
 void uart3_init(void);
 void USART3_IRQHandler(void);
 
