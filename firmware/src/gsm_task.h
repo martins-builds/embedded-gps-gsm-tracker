6b00c28f3f98a9ddf8bb53ee0201e894bf86c643
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-19 07:39:35 +0100

    send byte over uart

--- firmware/src/gsm_task.h
+++ firmware/src/gsm_task.h
@@ -12,5 +12,6 @@ void uart3_init(void);
 uint8_t buffer_ends_with(char *buf, uint16_t index, const char *suffix);
 void USART3_IRQHandler(void);
 void gsm_send_command(const char *cmd);
+uart3_send_byte(uint8_t byte);
 
 #endif
