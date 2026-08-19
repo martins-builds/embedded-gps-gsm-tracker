48a6cc4249dd8b5c7a40961bebc82c7bb57ed303
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-19 07:58:41 +0100

    Making the gsm semaphore based

--- firmware/src/gsm_task.h
+++ firmware/src/gsm_task.h
@@ -3,10 +3,12 @@
 
 #include "stm32l476re.h"
 #include "core_cm4.h"
+#include "semphr.h"
 
 #define GSM_BUF_SIZE 200
 extern char gsm_rx_buffer[GSM_BUF_SIZE];
 extern volatile uint8_t gsm_response_ready;
+extern SemaphoreHandle_t gsm_response_sem;
 
 void uart3_init(void);
 uint8_t buffer_ends_with(char *buf, uint16_t index, const char *suffix);
