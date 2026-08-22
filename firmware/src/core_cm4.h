5b69e17197409aba66933510ab101df8fb5f6f41
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-22 13:40:32 +0100

    Update core_cm4.h

--- firmware/src/core_cm4.h
+++ firmware/src/core_cm4.h
@@ -27,15 +27,15 @@ typedef struct {
      volatile uint32_t CPUID;
      volatile uint32_t ICSR;
      volatile uint32_t VTOR;
+     volatile uint32_t AIRCR;
+     volatile uint32_t SCR;
+     volatile uint32_t CCR;
+     volatile uint32_t SHPR1;
+     volatile uint32_t SHPR2;
+     volatile uint32_t SHPR3;
      volatile uint32_t SHCRS;
      volatile uint32_t CFSR;
      volatile uint32_t HFSR;
-     volatile uint32_t MMAR;
-     volatile uint32_t BFAR;
-     volatile uint32_t 
-     volatile uint32_t 
-     volatile uint32_t 
-     volatile uint32_t 
      volatile uint32_t 
      volatile uint32_t 
      volatile uint32_t 
