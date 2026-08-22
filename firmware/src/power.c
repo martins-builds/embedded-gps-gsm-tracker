8e672ae473c7a48160aaf4c02148036e1f79ae39
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-22 14:02:39 +0100

    found the sleep till exti

--- firmware/src/power.c
+++ firmware/src/power.c
@@ -3,4 +3,11 @@
 void enter_stop_mode(void){
     PWR->CR1 &= ~(3 << 0);
     PWR->CR1 |= (2 << 0); //using stop 2 mode
+
+    SCB->SCR |= (1 << 2); //sleepdeep
+
+    /*hard to find, keep sleeping till interrupt i.e power on
+    or distress signal*/
+    __asm volatile ("wfi"); 
+    
 }
