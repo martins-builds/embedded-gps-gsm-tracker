4eb516a9eb9d067c20e00444a69466c73483a87e
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-27 10:43:52 +0100

    all headers for main wired up

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,15 +1,11 @@
 #include "gps_task.h"
 #include "gsm_task.h"
+#include "clock.h"
+#include "gpio.h"
+#include "display_task.h"
 #include "power.h"
 #include "FreeRTOS.h"
-#include <stdio.h>
-#include <string.h>
 
 __attribute__((used)) void main(void){
-    while (1){
-        if (gps_data.valid){
-            gsm_send_location(&gps_data);
-        }
-        //enter_stop_mode();// not gonna use this for now
-    }
+    
 }
