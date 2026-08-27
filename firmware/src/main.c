bbf64e6a9815b7cf3138d34baa50e90790322ff1
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-27 10:45:40 +0100

    some init sequence and extra headers

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,9 +3,16 @@
 #include "clock.h"
 #include "gpio.h"
 #include "display_task.h"
+#include "rtc.h"
 #include "power.h"
 #include "FreeRTOS.h"
 
 __attribute__((used)) void main(void){
-    
+    clock_init();
+    gpio_init();
+    button_interrupt_init();
+    i2c_init();
+    uart1_init();
+    uart3_init();
+
 }
