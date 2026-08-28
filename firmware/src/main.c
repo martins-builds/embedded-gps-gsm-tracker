9968b2bb6601695fcb687c0c00bb7fa2bfaee8f0
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-28 10:37:04 +0100

    placing more space for display

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -23,7 +23,7 @@ __attribute__((used)) void main(void){
     //create tasks
     xTaskCreate(gsm_task, "GSM", 128, NULL, 1, NULL);
     xTaskCreate(gps_task, "GPM", 128, NULL, 1, NULL);
-    xTaskCreate(display_task, "DISPLAY", 128, NULL, 1, NULL);
+    xTaskCreate(display_task, "DISPLAY", 256, NULL, 1, NULL);
     vTaskStartScheduler();
     //need watchdog for program break
 }
