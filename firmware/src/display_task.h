9daec47eab202ab2f8e3bf0fbaca17e81b6676ac
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-30 08:40:40 +0100

    cleared bugs

--- firmware/src/display_task.h
+++ firmware/src/display_task.h
@@ -7,8 +7,8 @@
 
 void i2c_init(void);
 void OLED_DrawPixel(int16_t x, int16_t y, uint8_t color);
-void OLED_DrawChar(x, y, char c);
-void OLED_DrawString(x, y, char *str);
+void OLED_DrawChar(int16_t x, int16_t y, char c);
+void OLED_DrawString(int16_t x, int16_t y, char *str);
 void i2c_write(uint8_t dev_addr, uint8_t *data, uint16_t len);
 void ssd1306_init(void);
 void ssd1306_update_display(void);
