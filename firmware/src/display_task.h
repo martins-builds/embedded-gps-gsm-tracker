6d2c1210f4bdce66dbacfe38ce197c52ca620417
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-08-30 08:37:09 +0100

    declaring said functions in the headers

--- firmware/src/display_task.h
+++ firmware/src/display_task.h
@@ -7,6 +7,8 @@
 
 void i2c_init(void);
 void OLED_DrawPixel(int16_t x, int16_t y, uint8_t color);
+void OLED_DrawChar(x, y, char c);
+void OLED_DrawString(x, y, char *str);
 void i2c_write(uint8_t dev_addr, uint8_t *data, uint16_t len);
 void ssd1306_init(void);
 void ssd1306_update_display(void);
