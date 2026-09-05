0ac5dc0ec555f481f2c98c7eb878d5a87201cf78
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-05 10:48:26 +0100

    mutex

--- firmware/src/gps_task.c
+++ firmware/src/gps_task.c
@@ -6,6 +6,7 @@ char gps_rx_buffer[GPS_BUF_SIZE];
 volatile uint8_t gps_line_ready = 0;
 static uint16_t gps_rx_index = 0;
 GPS_Data_t gps_data;
+MutexHandle_t gps_data_mutex;
 
 void gps_parse_gprmc(const char *sentence, GPS_Data_t *result){
     char sentence_copy[100];
