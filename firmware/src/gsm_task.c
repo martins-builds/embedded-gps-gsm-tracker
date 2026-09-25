8117ab0d9b5c8517563d79eb964e09f5ff0ef52c
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-25 14:53:36 +0100

    Update gsm_task.c

--- firmware/src/gsm_task.c
+++ firmware/src/gsm_task.c
@@ -59,7 +59,9 @@ void USART3_IRQHandler(void){
             BaseType_t xHigherPriorityTaskWoken = pdFALSE;
             xSemaphoreGiveFromISR(gsm_response_sem, &xHigherPriorityTaskWoken);
             gsm_rx_index = 0;
-            strstr(, "OK")
+            portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+            gsm_rx_buffer[gsm_rx_index] = '\0';
+            strstr(gsm_rx_buffer, "OK");
         }
     }
     else{
